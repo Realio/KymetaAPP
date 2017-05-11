85e19ea02cdd0abbb568ea0ba98644274a5e312d
Jeff <realio@gmail.com>
2017-05-10 21:32:09 -0700

    Add files via upload

--- index.android.js
+++ index.android.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import {AppRegistry,StyleSheet,Text,View} from 'react-native';
 import Header from './src/components/Header';
 import Body from './src/components/Body';
+import AlbumList from './src/components/AlbumList';
+
 
 export default class KymetaTimeKeeper extends Component {
   render() {
     return (
-      <View>
+      <View style= {{flex: 1}}>
         <Header headerText={'Kymeta Time Keeper Aid'}/>
-        <Body/>
+        <AlbumList/>
       </View>
     );
   }
