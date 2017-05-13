fe0599a70994663b18ca28445591a4cbfd2b675b
Jeff <realio@gmail.com>
2017-05-13 16:33:39 -0700

    Add files via upload

--- index.android.js
+++ index.android.js
@@ -1,21 +1,44 @@
 
 import React, { Component } from 'react';
-import {AppRegistry,StyleSheet,Text,View} from 'react-native';
+import {AppRegistry,StyleSheet,Text,View,Alert,TouchableHighlight} from 'react-native';
 import Header from './src/components/Header';
 import Body from './src/components/Body';
-import AlbumList from './src/components/AlbumList';
+import MyPicker from './src/components/MyPicker';
 
+var CHAMBERS = ['Brick', 'Ellie', 'Hertz']
+var alertMessage = 'I hate Khoa';
 
-export default class KymetaTimeKeeper extends Component {
+export default class KymetaAPP extends Component {
   render() {
     return (
       <View style= {{flex: 1}}>
         <Header headerText={'Kymeta Time Keeper Aid'}/>
-        <AlbumList/>
+        <Text> {CHAMBERS[2]} </Text>
+        <Body/>
+        <TouchableHighlight style={styles.wrapper}
+          onPress={() => Alert.alert(
+            'WARNING',
+            alertMessage,
+          )}>
+          <View style={styles.button}>
+            <Text>Click here for important information</Text>
+          </View>
+        </TouchableHighlight>
+        <MyPicker/>
       </View>
     );
   }
 }
 
+var styles = StyleSheet.create({
+  wrapper: {
+    borderRadius: 5,
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: '#eeeeee',
+    padding: 10,
+  },
+});
 
-AppRegistry.registerComponent('KymetaTimeKeeper', () => KymetaTimeKeeper);
+AppRegistry.registerComponent('KymetaAPP', () => KymetaAPP);
---
+++ index.android1.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+import {AppRegistry,StyleSheet,Text,View} from 'react-native';
+import Header from './src/components/Header';
+import Body from './src/components/Body';
+import AlbumList from './src/components/AlbumList';
+
+
+export default class KymetaAPP extends Component {
+  render() {
+    return (
+      <View style= {{flex: 1}}>
+        <Header headerText={'Kymeta Time Keeper Aid'}/>
+        <AlbumList/>
+      </View>
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('KymetaAPP', () => KymetaAPP);
