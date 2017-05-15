bbbe5b6b3a417a8571d7df961f56134cd47ffca5
Jeff <realio@gmail.com>
2017-05-14 19:02:50 -0700

    Add files via upload

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,4 @@
 export const INCREMENT = 'INCREMENT';
+export const RESETCOUNTER = 'RESETCOUNTER';
+export const MULTIPLIER = 'MULTIPLIER';
+export const CREATE_RUN = 'CREATE_RUN';
--- src/actions/appActions.js
+++ src/actions/appActions.js
@@ -1,8 +1,33 @@
 import * as types from './actionTypes';
 
-export function increment() {
+export function increment(amount = 1) {
   console.log('creating increment action');
   return {
-    type: types.INCREMENT
+  type: types.INCREMENT,
+  amount
+  };
+}
+
+export function resetcounter() {
+  console.log('hard reset on counter');
+  return {
+    type: types.RESETCOUNTER
+  };
+}
+
+export function multiplier(multiplier) {
+  console.log('trying to multiply some shit');
+  return {
+    type: types.MULTIPLIER,
+    multiplier: multiplier
+  };
+}
+
+export function createRun(chamber, type, duration) {
+  return {
+    type: types.CREATE_RUN,
+    chamber: chamber,
+    type: type,
+    duration: duration
   };
 }
