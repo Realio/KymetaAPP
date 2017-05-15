2e52c489d55bf5a63f07786440651cc0d51670e8
Jeff <realio@gmail.com>
2017-05-14 19:03:17 -0700

    Add files via upload

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,7 +1,10 @@
 import * as types from '../actions/actionTypes';
 
 const initialState = {
-  counter: 0
+  counter: 5,
+  chamber: 'Choose Chamber',
+  type: 'Choose Type',
+  duration: 'Choose Duration'
 };
 
 export default function handleAction(state = initialState, action = {}) {
@@ -13,8 +16,36 @@ export default function handleAction(state = initialState, action = {}) {
       // counter with a new value
       return {
         ...state,
-        counter: state.counter + 1
+        counter: state.counter + action.amount
       };
+
+    case types.RESETCOUNTER:
+      console.log('resetting');
+      //resetting my counter object back to initial value
+      return {
+        ...state,
+        counter: initialState.counter
+      };
+
+    case types.MULTIPLIER:
+      console.log('they multiplying yo');
+      //trying to multiply some shizz
+      return {
+        ...state,
+        counter: state.counter * action.multiplier
+      };
+
+    case types.CREATE_RUN:
+      console.log('executing my dropdown data');
+        //attempting to parse data from dropdowns
+      return {
+        ...state,
+        chamber,
+        type,
+        duration
+      };
+    case undefined:
+      return {...initialState, state};
 
     default:
       return state;
